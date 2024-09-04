0e218d791e41e51b31b8875d014bb7a46ad42978
Ankit Saroj <ankitsaroj.ankit94555@gmail.com>
2024-09-04 12:58:13 +0530

    new programm added odd and even

---
+++ C++/odd_even.cpp
@@ -0,0 +1,22 @@
+#include<iostream  >
+using namespace std;
+int main(){
+
+int number;
+
+cout<<"enter the number:";
+cin>>number;
+if(
+    number%2==0
+
+){
+    cout<<"odd";
+
+}
+
+else{
+    cout<<"even";
+}
+return 0;
+}
+

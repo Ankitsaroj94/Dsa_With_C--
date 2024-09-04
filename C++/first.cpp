3b48314a821ab94ab8e5eaae2fb9389244cecc55
Ankit Saroj <ankitsaroj.ankit94555@gmail.com>
2024-09-04 12:18:51 +0530

    initial commit

---
+++ C++/first.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+//swap two numb 
+
+    int a,b,temp;
+   
+
+cout<<" Enter value of a :";
+cin>>a;
+cout<<" Enter value of b :";
+cin>>b;
+
+
+ a=b;
+ b=temp;
+ temp=a;
+
+
+cout<<"Swapped value of a and b "<<endl;
+cout<<"a:"<<a <<endl <<"b:"<<b;
+return 0;
+}
+
+
+
